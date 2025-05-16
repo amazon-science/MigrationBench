d46ee372bd7cb5958192aceb64f04e5d33561479
Xinle Sheila Liu <sliuxl@amazon.com>
2025-05-15 17:42:23 -0700

    nit: Fix requirements.txt and test files list.

--- src/migration_bench/run_test_cmd.sh
+++ src/migration_bench/run_test_cmd.sh
@@ -3,9 +3,9 @@ set -x
 python ./common/test_file_utils.py
 python ./common/test_git_repo.py
 python ./common/test_hash_utils.py
+python ./common/test_hf_utils.py
 python ./common/test_maven_utils.py
 python ./common/test_utils.py
-python ./datasets/test_hf_utils.py
 python ./eval/test_final_eval.py
 python ./eval/test_repos.py
 python ./lang/java/eval/test_parse_file.py
