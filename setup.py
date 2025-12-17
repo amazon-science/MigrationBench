9b08623cf22d1fb3ec29b541464ce95ef68cc0e7
Bryan Lu <luyuzhe111@users.noreply.github.com>
2025-12-17 23:34:28 +0000

    add flexible dependencies to setup.py

--- setup.py
+++ setup.py
@@ -14,4 +14,17 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     python_requires=">=3.9",
+    install_requires=[
+        "datasets>=3.0.0",
+        "gitpython>=3.1.0",
+        "javalang>=0.13.0",
+        "numpy>=1.26.0",
+        "parameterized>=0.8.0",
+        "protobuf>=3.20.0",
+        "pydantic>=2.0.0",
+        "pylint>=2.14.0",
+        "pytz>=2024.1",
+        "packaging>=21.0",
+        "requests>=2.28.0",
+    ],
 )
